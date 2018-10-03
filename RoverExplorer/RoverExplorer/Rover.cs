3ea505e45cacb93b75b7db87a522e1b01e751d75
Georgiev, Stefan (Developer) <stefan.georgiev@sky.uk>
2018-10-03 12:46:48 +0100

    adding first test - move forward

--- RoverExplorer/RoverExplorer/Rover.cs
+++ RoverExplorer/RoverExplorer/Rover.cs
@@ -14,15 +14,35 @@ namespace RoverExplorer
 
     public class Rover
     {
-        CompassDirection _currentOrientation;
-
+        ITerrainGrid _planetTerrainGrid;
+        
         readonly GridPoint _cussrentGridPosition;
 
         public Rover(ITerrainGrid terrain)
         {
-            _currentOrientation = CompassDirection.NORTH;
+            _planetTerrainGrid = terrain;
+            CompassCurrentOrientation = CompassDirection.NORTH;
             _cussrentGridPosition = new GridPoint(0,0);
         }
+        public Rover(ITerrainGrid terrain, GridPoint initialPosition, CompassDirection orientation) 
+            : this(terrain)
+        {
+            CompassCurrentOrientation = orientation;
+            _cussrentGridPosition = initialPosition;
+        }
 
+        public CompassDirection CompassCurrentOrientation { get; private set; }
+
+        public GridPoint MoveForward()
+        {
+            switch (CompassCurrentOrientation)
+            {
+                case CompassDirection.NORTH:
+                    return new GridPoint(_cussrentGridPosition.X + 1, _cussrentGridPosition.Y);
+                default:
+                    return _cussrentGridPosition;
+            }
+            
+        }
     }
 }
--- RoverExplorer/RoverExplorerTest/RoverTest.cs
+++ RoverExplorer/RoverExplorerTest/RoverTest.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RoverExplorer;
+using RoverExplorer.PlanetRepresentation;
 
 namespace RoverExplorerTest
 {
     [TestClass]
     public class RoverTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        Mock<ITerrainGrid> terrainGrid;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            terrainGrid = new Mock<ITerrainGrid>();
+        }
+
+        [TestMethod]
+        public void TestMoveForward()
+        {
+            var rover = new Rover(terrainGrid.Object, new GridPoint(0, 0), CompassDirection.NORTH);
+
+            var newPosition = rover.MoveForward();
+
+            Assert.IsTrue(newPosition.X == 1 && newPosition.Y == 0);
+            Assert.IsTrue(rover.CompassCurrentOrientation == CompassDirection.NORTH);
         }
     }
 }
