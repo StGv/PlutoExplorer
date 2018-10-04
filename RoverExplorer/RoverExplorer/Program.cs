b1f147aa8c149806e2caf967260a096c35da6154
Georgiev, Stefan (Developer) <stefan.georgiev@sky.uk>
2018-10-04 18:21:07 +0100

    implementing actually the console applucation to invoke the API

--- RoverExplorer/RoverExplorer/Program.cs
+++ RoverExplorer/RoverExplorer/Program.cs
@@ -1,8 +1,5 @@
+using RoverExplorer.PlanetRepresentation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RoverExplorer
 {
@@ -10,7 +7,34 @@ namespace RoverExplorer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int MaxX = new Random().Next(0, 100);
+            int MaxY = new Random().Next(0, 100);
+
+            var rover = new Rover(new TerrainGrid(MaxX, MaxY));
+
+            string input = Console.ReadLine();
+            foreach(char letter in input)
+            {
+                switch (letter)
+                {
+                    case (char)Directions.Forward:
+                        rover.MoveForward();
+                        Console.WriteLine($"Moving to {rover.CurrentGridPosition.X}, {rover.CurrentGridPosition.Y} facing {rover.CompassCurrentOrientation}");
+                        break;
+                    case (char)Directions.Backward:
+                        rover.MoveBackward();
+                        Console.WriteLine($"Moving to {rover.CurrentGridPosition.X}, {rover.CurrentGridPosition.Y} facing {rover.CompassCurrentOrientation}");
+                        break;
+                    case (char)Directions.Left:
+                        rover.TurnLeft();
+                        Console.WriteLine($"Moving to {rover.CurrentGridPosition.X}, {rover.CurrentGridPosition.Y} facing {rover.CompassCurrentOrientation}");
+                        break;
+                    case (char)Directions.Right:
+                        rover.TurnRight();
+                        Console.WriteLine($"Moving to {rover.CurrentGridPosition.X}, {rover.CurrentGridPosition.Y} facing {rover.CompassCurrentOrientation}");
+                        break;
+                }
+            }
             Console.ReadKey();
         }
     }
